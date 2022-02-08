10dec5af04cee0370013c30a6a1df0620b2b214a
raddcircles <raddcircles@gmail.com>
2022-02-08 03:31:09 -0500

    Add project files.

---
+++ SomniumDebugEnabler/DebugEnabler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using MelonLoader;
+using Game;
+using SLua;
+using HarmonyLib;
+
+namespace SomniumDebugEnabler
+{
+
+    [HarmonyPatch(typeof(ScriptManager), "Load")]
+    class Patch2
+    {
+        public static void Postfix()
+        {
+            DebugEnabler.logger.Msg(LuaState.main["USER_NAME"]);
+        }
+    }
+
+    [HarmonyPatch(typeof(LuaState), "setObject", new Type[] {
+    typeof (string), typeof (object), typeof (bool), typeof (bool)
+  })]
+    class Patch
+    {
+        public static void Prefix(LuaState __instance, string key, ref object v)
+        {
+            if (key == "UNITY_EDITOR")
+            {
+                v = DebugEnabler.EDITOR.Value;
+            }
+            if (key == "UNITY_STANDALONE_WIN")
+            {
+                v = DebugEnabler.STANDALONE_WIN.Value;
+            }
+            if (key == "UNITY_PS4")
+            {
+                v = DebugEnabler.PS4.Value;
+            }
+            if (key == "UNITY_XBOXONE")
+            {
+                v = DebugEnabler.XBOXONE.Value;
+            }
+            if (key == "UNITY_SWITCH")
+            {
+                v = DebugEnabler.SWITCH.Value;
+            }
+            if (key == "BUILD_RELEASE")
+            {
+                v = DebugEnabler.BUILD_RELEASE.Value;
+            }
+            if (key == "BUILD_REGION")
+            {
+                v = DebugEnabler.BUILD_REGION.Value;
+            }
+            if (key == "USER_NAME")
+            {
+                v = DebugEnabler.USERNAME.Value;
+            }
+        }
+
+    }
+    public class DebugEnabler : MelonMod
+    {
+        public static MelonLogger.Instance logger;
+        public static MelonPreferences_Category overCat;
+        public static MelonPreferences_Entry<int> EDITOR;
+        public static MelonPreferences_Entry<int> STANDALONE_WIN;
+        public static MelonPreferences_Entry<int> PS4;
+        public static MelonPreferences_Entry<int> XBOXONE;
+        public static MelonPreferences_Entry<int> SWITCH;
+        public static MelonPreferences_Entry<int> BUILD_RELEASE;
+        public static MelonPreferences_Entry<string> BUILD_REGION;
+        public static MelonPreferences_Entry<string> USERNAME;
+
+        public override void OnApplicationStart()
+        {
+            LoggerInstance.Msg("Mod Loaded.");
+            logger = LoggerInstance;
+            overCat = MelonPreferences.CreateCategory("Overrides");
+            EDITOR = overCat.CreateEntry("UNITY_EDITOR", 0);
+            STANDALONE_WIN = overCat.CreateEntry("UNITY_STANDALONE_WIN", 1);
+            PS4 = overCat.CreateEntry("UNITY_PS4", 0);
+            XBOXONE = overCat.CreateEntry("UNITY_XBOXONE", 0);
+            SWITCH = overCat.CreateEntry("UNITY_SWITCH", 0);
+            BUILD_RELEASE = overCat.CreateEntry("BUILD_RELEASE", 0);
+            BUILD_REGION = overCat.CreateEntry("BUILD_REGION", "BUILD_WORLDWIDE");
+            USERNAME = overCat.CreateEntry("USER_NAME", "gonzo");
+
+        }
+    }
+}
---
+++ SomniumDebugEnabler/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using SomniumDebugEnabler;
+
+//ml stuff
+[assembly: MelonInfo(typeof(DebugEnabler), "AI: The Somnium Files Debug Mode Enable", "1", "Slavanomics", "https://github.com/slavanomics/SomniumDebugEnabler")]
+[assembly: MelonGame("SpikeChunsoft", "AI_TheSomniumFiles")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SomniumDebugEnabler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SomniumDebugEnabler")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4d92c1b-d862-4a5b-9648-aff0507982c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
